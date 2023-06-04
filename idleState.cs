64b65cc3442ee3d3a4ddb224717ad74d82d95bd0
loyaldudee <115097956+loyaldudee@users.noreply.github.com>
2023-06-04 13:23:45 +0530

    Commited to improve enemy AI
    I guess the title explains it

---
+++ idleState.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class idleState : StateMachineBehaviour
+{
+    float timer;
+    Transform Player;
+    float chaseRange = 8;
+    // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
+    override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        timer = 0;
+        Player = GameObject.FindGameObjectWithTag("Player").transform;
+    }
+
+    // OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
+    override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        timer += Time.deltaTime;
+        if(timer > 5)
+            animator.SetBool("IsPatroling",true);
+        float distance = Vector3.Distance(Player.position, animator.transform.position);
+        if (distance < chaseRange)
+            animator.SetBool("IsChasing",true);
+    } 
+
+    // OnStateExit is called when a transition ends and the state machine finishes evaluating this state
+    override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+       
+    }
+
+    // OnStateMove is called right after Animator.OnAnimatorMove()
+    //override public void OnStateMove(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    //{
+    //    // Implement code that processes and affects root motion
+    //}
+
+    // OnStateIK is called right after Animator.OnAnimatorIK()
+    //override public void OnStateIK(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    //{
+    //    // Implement code that sets up animation IK (inverse kinematics)
+    //}
+}
